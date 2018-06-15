8a1bb4f699519489629b1d27239878bf0598c00f
Jacob Turnbull <djjaketurnbull@gmail.com>
2018-06-14 21:46:01 -0500

    Init

---
+++ WpfTestApp/WpfTestApp/Models/TestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestApp.Models
+{
+    public class TestModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name {
+            get { return _name; }
+            set {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        private string _value;
+        public string Value {
+            get { return _value; }
+            set {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfTestApp/WpfTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WpfTestApp.Models;
+
+namespace WpfTestApp.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<TestModel> _testList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<TestModel> TestList
+        {
+            get
+            {
+                return _testList;
+            }
+            set
+            {
+                _testList = value;
+            }
+        }
+
+        #region ctor
+        public MainWindowViewModel()
+        {
+            InitData();
+            TestList.CollectionChanged += TestList_CollectionChanged; ;
+        }
+
+        private void TestList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            
+        }
+
+        void TestModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "ListGrid")
+            {
+
+            }
+        }
+
+        private void InitData()
+        {
+            _testList = new ObservableCollection<TestModel>() {
+                new TestModel() { Name = "testname1", Value = "testvalue1" },
+                new TestModel() { Name = "testname2", Value = "testvalue2" },
+            };
+        }
+        #endregion
+    }
+}
